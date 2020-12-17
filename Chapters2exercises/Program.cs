59fcff9ae9dfc206d86e823dc01b468b0156b9ff
Julie Rindone <julie.rindone@gmail.com>
2020-12-17 14:01:22 -0600

    Add project files.

---
+++ Chapters2exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapters2exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
